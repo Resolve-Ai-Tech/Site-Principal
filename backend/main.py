d818e07ffbec914a3c132a05e3b730c07ce315b0
Se7enzito <bernardocastro2006@gmail.com>
2024-08-22 21:06:48 -0300

    Atualização Site

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_session import Session
+
+app = Flask(__name__, template_folder="../frontend/templates")
+
+app.static_folder = '../frontend/src'
+app.static_url_path = '/static'
+
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+if __name__ == '__main__':    
+    app.run(debug=True, port=8000)
